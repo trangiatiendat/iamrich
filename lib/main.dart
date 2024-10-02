db15245743dd53e903a3dfbeca47cd04a3cbe145
trangiatiendat <tgtd2k3@gmail.com>
2024-10-03 01:06:29 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ImRich());
+}
+
+class ImRich extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Center(
+            child: Text(
+              'I AM RICH!',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 3.0,
+              ),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
